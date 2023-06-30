669e00bad50b825f03732ad7b3c13b58ee8b6f91
SimoneFilograna <simonefilogranaf12@gmail.com>
2023-06-30 14:32:55 +0200

    fixed-js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,21 @@
 const kmRoute = parseInt( prompt ("Quanti km devi percorrere?"));
 const age = parseInt( prompt ("Quanti anni hai?"));
-const price = (kmRoute * 0,21);
+const price = (kmRoute * 0.21);
+const priceResult = price.toFixed(2);
 
 
 // offerta minori
 const minOffer = (price - (price * 0.20));
+const minResult = minOffer.toFixed(2);
+
+//min decimal
+console.log("prezzo scontato 2 decimali");
+console.log(minResult);
 
 
 //offerta pensionati
 const maxOffer = (price - (price * 0.40));
+const maxResult = maxOffer.toFixed(2);
 
 
 console.log(kmRoute, age);
@@ -22,9 +29,9 @@ console.log(maxOffer);
 
 
 if (age < 18) {
-    document.getElementById("ticket-price").innerHTML = minOffer;   
+    document.getElementById("ticket-price").innerHTML = minResult;   
 }   else if (age >= 65) {
-    document.getElementById("ticket-price").innerHTML = maxOffer;
+    document.getElementById("ticket-price").innerHTML = maxResult;
 }   else {
-    document.getElementById("ticket-price").innerHTML = price;
+    document.getElementById("ticket-price").innerHTML = priceResult;
 }
